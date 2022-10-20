99d31792fff3b81e2e3a13e1402a815c3070042c
Varun Singhal <82788859+varuns31@users.noreply.github.com>
2022-10-20 04:51:14 +0000

    Register

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 const express = require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+  res.render('index.ejs',{ status: 'Varun'})
+})
+app.get('/login',(req,res)=>{
+  res.render('login.ejs',{ status: 'Varun'})
+})
+app.get('/register',(req,res)=>{
+  res.render('register.ejs',{ status: 'Varun'})
+})
+app.post('/register',(req,res)=>{
+  
+})
+
+app.listen(3000)
