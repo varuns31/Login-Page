886258697f8dde01bcfbbe94958b13f86a3bbb40
Varun Singhal <82788859+varuns31@users.noreply.github.com>
2022-10-20 05:10:30 +0000

    Register implemented

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
+const users=[]
+
+app.use(express.urlencoded({extended:false}))
 
 app.get('/',(req,res)=>{
   res.render('index.ejs',{ status: 'Varun'})
@@ -7,11 +11,26 @@ app.get('/',(req,res)=>{
 app.get('/login',(req,res)=>{
   res.render('login.ejs',{ status: 'Varun'})
 })
+app.post('/login',(req,res)=>{
+  
+})
 app.get('/register',(req,res)=>{
   res.render('register.ejs',{ status: 'Varun'})
 })
-app.post('/register',(req,res)=>{
-  
+app.post('/register',async(req,res)=>{
+  try{
+    const hashedpassword=await bcrypt.hash(req.body.password,10)
+    users.push(
+      {
+        id:Date().now().toString(),
+        name:req.body.name,
+        email:req.body.email,
+        password:hashedpassword
+      })
+      res.redirect('/login')
+  }catch{
+    
+  }
 })
 
 app.listen(3000)
